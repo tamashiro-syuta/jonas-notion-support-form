b3040345f82dfb739750425cb71fb645606be6e8
tamashiro-syuta <syuta5825@gmail.com>
2024-03-17 17:27:32 +0900

    ✨ ユーザー情報は /user で表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,7 @@
-"use client";
-
-import Image from "next/image";
-import { selectors } from "@/components/custom/LiffProvider/store";
-
 export default function Home() {
-  const user = selectors.useUser();
-
-  console.log("user", user);
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {user ? (
-        <div>
-          <h1 className="text-3xl font-bold">
-            こんにちは、{user.displayName}さん
-          </h1>
-          <p className="text-lg">ID: {user.userId}</p>
-          <p className="text-lg">MESSAGE: {user.statusMessage}</p>
-          {user.pictureUrl && (
-            <Image
-              src={user.pictureUrl}
-              alt={user.displayName}
-              width={400}
-              height={400}
-            />
-          )}
-        </div>
-      ) : (
-        <p>未ログイン</p>
-      )}
+      <p>this is Top page.</p>
     </main>
   );
 }
