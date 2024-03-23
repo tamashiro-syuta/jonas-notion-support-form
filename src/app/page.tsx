1191a7ec8107a36a2702c762975a9185db089e20
tamashiro-syuta <syuta5825@gmail.com>
2024-03-23 19:50:00 +0900

    ✨ ルートに飛ぶのはliffでログインした時のれダイレクトのみなので、ローディングを表示しとく

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,6 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import Loading from "@/components/custom/Loading";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <Button
-        asChild
-        className="flex-1 flex items-center justify-center bg-red-400 border-8 hover:border-background rounded-xl w-full hover:bg-white hover:text-red-400 hover:border-red-400 hover:rounded-xl"
-      >
-        <Link href="/payment" className="text-9xl">
-          支出
-        </Link>
-      </Button>
-
-      <Button
-        asChild
-        className="flex-1 flex items-center justify-center bg-blue-400 border-8 hover:border-background rounded-xl w-full hover:bg-white hover:text-blue-400 hover:border-blue-400 hover:rounded-xl"
-      >
-        <Link href="/hoge" className="text-9xl">
-          表示
-        </Link>
-      </Button>
-    </main>
-  );
+  // NOTE: ルートに遷移するのは、ログイン後にリダイレクトのみで、その後も別ページへ遷移されるので、ローディングUIを表示しておく
+  return <Loading />;
 }
