efb2141aa848bc8fccd006ee983cd4dd63d8453f
tamashiro-syuta <syuta5825@gmail.com>
2024-04-07 11:19:08 +0900

    ✨ 設定画面にもデフォルトのDBにアクセスできるページを追加

---
+++ src/app/settings/budgetByGenre/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useCallback, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { fetchDefaultNotionDB } from "@/app/actions/db/notionDB";
+import { useLiff } from "@/components/custom/LiffProvider";
+import Loading from "@/components/custom/Loading";
+import { showError } from "@/lib/toast-actions";
+
+const Page = () => {
+  const router = useRouter();
+  const { liff, user } = useLiff();
+
+  const fetchAndSetBudget = useCallback(async () => {
+    if (!liff || !user) {
+      const message = "まずは右上のアイコンボタンからログインしようか";
+      showError({ message });
+      return;
+    }
+
+    try {
+      const db = await fetchDefaultNotionDB({ userID: user.userId });
+
+      router.push(`/settings/${db.id}/budgetByGenre`);
+    } catch (error) {
+      showError({ message: `${error}`, duration: 50000 });
+      router.push("/");
+    }
+  }, [liff, router, user]);
+
+  useEffect(() => {
+    fetchAndSetBudget();
+  }, [fetchAndSetBudget]);
+
+  return <Loading />;
+};
+
+export default Page;
