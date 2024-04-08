5588d0b401757a2265e496f7e21902605318a658
tamashiro-syuta <syuta5825@gmail.com>
2024-04-08 16:32:23 +0900

    ✨ ルーティングの変更に応じて、サイドバーのリンクも変更

--- src/components/custom/app-bar/index.tsx
+++ src/components/custom/app-bar/index.tsx
@@ -1,5 +1,28 @@
-import AppBarCompoent from "./server";
+import Link from "next/link";
+import { Menubar, MenubarMenu } from "@/components/ui/menubar";
+import AccountMenu from "./account-menu";
+import SideBar from "./side-bar";
 
-const AppBar = () => <AppBarCompoent />;
+function AppBar() {
+  return (
+    <Menubar className="sticky top-0 z-50 rounded-none w-full h-14 bg-primary flex justify-between">
+      <div className="flex items-center">
+        <MenubarMenu>
+          <SideBar />
+        </MenubarMenu>
+        <MenubarMenu>
+          <Link href="/">
+            <h1 className="text-base text-white font-bold">かけちゃん</h1>
+          </Link>
+        </MenubarMenu>
+      </div>
+
+      {/* NOTE: 右寄せメニュー */}
+      <div>
+        <AccountMenu />
+      </div>
+    </Menubar>
+  );
+}
 
 export default AppBar;
---
+++ src/components/custom/app-bar/side-bar.tsx
@@ -0,0 +1,152 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import clsx from "clsx";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import {
+  GearIcon,
+  ChevronRightIcon,
+  HamburgerMenuIcon,
+  PaperPlaneIcon,
+} from "@radix-ui/react-icons";
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+import { HouseholdType } from "@prisma/client";
+
+export const SideBar = () => {
+  const INDIVIDUAL_HOUSEHOLD_NAME = "個人家計簿";
+  const PAIRS_HOUSEHOLD_NAME = "同棲家計簿";
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+
+  const householdType = isChecked
+    ? HouseholdType.PAIRS
+    : HouseholdType.INDIVIDUAL;
+
+  const isPairs = householdType === HouseholdType.PAIRS;
+  const selectedHouseholdType = isPairs
+    ? PAIRS_HOUSEHOLD_NAME
+    : INDIVIDUAL_HOUSEHOLD_NAME;
+  const unselectedHouseholdType = isPairs
+    ? INDIVIDUAL_HOUSEHOLD_NAME
+    : PAIRS_HOUSEHOLD_NAME;
+
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <HamburgerMenuIcon className="w-8 h-8 py-2 mx-2 text-white" />
+      </SheetTrigger>
+      <SheetContent
+        side="left"
+        className={clsx(isPairs ? "bg-cyan-50" : "bg-neutral-100")}
+      >
+        <SheetHeader className="text-left px-2">
+          <Label className="text-2xl">{selectedHouseholdType}</Label>
+          <div className="flex items-center justify-end space-x-2 pt-2">
+            <Label className="text-sm">{`${unselectedHouseholdType} へ`}</Label>
+            <Switch
+              checked={isPairs}
+              onCheckedChange={setIsChecked}
+              className={clsx(
+                isPairs
+                  ? "data-[state=checked]:bg-cyan-800"
+                  : "data-[state=unchecked]:bg-neutral-800"
+              )}
+            />
+          </div>
+        </SheetHeader>
+        <SheetHeader
+          className={clsx(
+            "mt-6 text-xl text-left px-2 py-2 mb-1 border-l-4 bg-white",
+            isPairs ? "border-cyan-800" : "border-neutral-800"
+          )}
+        >
+          <div className="flex items-center">
+            <PaperPlaneIcon className="h-6 w-6" />
+            <h2 className="pl-2 text-xl text-left">LINE通知</h2>
+          </div>
+        </SheetHeader>
+        <SheetClose asChild className="w-full">
+          <Link href={`/${householdType}/addSpending`}>
+            <div className="flex w-full items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-white dark:hover:bg-gray-700 group">
+              <div className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white">
+                <ChevronRightIcon className="w-full h-full" />
+              </div>
+              <span className="ms-3">支出の追加</span>
+            </div>
+          </Link>
+        </SheetClose>
+        <SheetClose asChild className="w-full">
+          <Link href={`/${householdType}/balanceByGenre`}>
+            <div className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-white dark:hover:bg-gray-700 group">
+              <div className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white">
+                <ChevronRightIcon className="w-full h-full" />
+              </div>
+              <span className="ms-3">項目別の予算</span>
+            </div>
+          </Link>
+        </SheetClose>
+        <SheetClose asChild className="w-full">
+          <Link href={`/${householdType}/budgetByGenre`}>
+            <div className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-white dark:hover:bg-gray-700 group">
+              <div className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white">
+                <ChevronRightIcon className="w-full h-full" />
+              </div>
+              <span className="ms-3">今月の残額</span>
+            </div>
+          </Link>
+        </SheetClose>
+
+        <SheetHeader
+          className={clsx(
+            "mt-6 text-xl text-left px-2 py-2 mb-1 border-l-4 bg-white",
+            isPairs ? "border-cyan-800" : "border-neutral-800"
+          )}
+        >
+          <div className="flex items-center">
+            <GearIcon className="h-6 w-6" />
+            <h2 className="pl-2 text-xl text-left">設定</h2>
+          </div>
+        </SheetHeader>
+        <SheetClose asChild className="w-full">
+          <Link href={`/${householdType}/addSpending/settings`}>
+            <div className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-white dark:hover:bg-gray-700 group">
+              <div className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white">
+                <ChevronRightIcon className="w-full h-full" />
+              </div>
+              <span className="ms-3">支出の追加</span>
+            </div>
+          </Link>
+        </SheetClose>
+        <SheetClose asChild className="w-full">
+          <Link href={`/${householdType}/budgetByGenre/settings`}>
+            <div className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-white dark:hover:bg-gray-700 group">
+              <div className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white">
+                <ChevronRightIcon className="w-full h-full" />
+              </div>
+              <span className="ms-3">項目別の予算</span>
+            </div>
+          </Link>
+        </SheetClose>
+        <SheetClose asChild className="w-full">
+          <Link href={`/${householdType}/balanceByGenre/settings`}>
+            <div className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-white dark:hover:bg-gray-700 group">
+              <div className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white">
+                <ChevronRightIcon className="w-full h-full" />
+              </div>
+              <span className="ms-3">今月の残額</span>
+            </div>
+          </Link>
+        </SheetClose>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default SideBar;
