8b6d806b284e66e57186c0e520113e3af2c72093
tamashiro-syuta <syuta5825@gmail.com>
2024-04-09 00:32:47 +0900

    🐛 タイポでした〜〜

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const ADD_SPENDING = "支出の記録" as const;
 export const BUDGET_BY_GENRE = "今日の予算" as const;
-export const BALANCE_BY_GENRE = "今日の残り" as const;
+export const BALANCE_BY_GENRE = "今月の残り" as const;
 export const SETTINGS = "設定" as const;
 
 export const INDIVIDUAL_HOUSEHOLD_NAME = "個人家計簿" as const;
